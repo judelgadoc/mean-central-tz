f168cf59cb62f3a3333297994bac143f52e0d025
Juan Pablo Delgado <judelgadoc@unal.edu.co>
2025-06-17 22:57:34 +0800

    gdal fix

--- mean-central-tz/src/main.rs
+++ mean-central-tz/src/main.rs
@@ -6,8 +6,8 @@ use std::env;
 #[derive(Debug)]
 struct Country {
     total_population: f64,
-    mean_lat: f64,
-    mean_lon: f64,
+    weighted_lat: f64,
+    weighted_lon: f64,
 }
 
 fn process_geotiff(path: &Path) -> Result<Country, Box<dyn std::error::Error>> {
@@ -18,8 +18,8 @@ fn process_geotiff(path: &Path) -> Result<Country, Box<dyn std::error::Error>> {
     let geo_transform = dataset.geo_transform()?;
 
     let mut total_population = 0.0;
-    let mut mean_lon = 0.0;
-    let mut mean_lat = 0.0;
+    let mut weighted_lat = 0.0;
+    let mut weighted_lon = 0.0;
 
     let buffer = band.read_as::<f32>(
         (0, 0),
@@ -44,16 +44,16 @@ fn process_geotiff(path: &Path) -> Result<Country, Box<dyn std::error::Error>> {
             let lat = geo_transform[3] + (j as f64) * geo_transform[5];
 
             total_population += value;
-            mean_lon += lon * value;
-            mean_lat += lat * value;
+            weighted_lat += lat * value;
+            weighted_lon += lon * value;
         }
     }
 
 
     let country = Country {
         total_population,
-        mean_lon: mean_lon / total_population,
-        mean_lat: mean_lat / total_population,
+        weighted_lat: weighted_lat / total_population,
+        weighted_lon: weighted_lon / total_population,
     };
 
     Ok(country)
@@ -62,6 +62,9 @@ fn process_geotiff(path: &Path) -> Result<Country, Box<dyn std::error::Error>> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<_> = env::args().collect();
     let result = process_geotiff(Path::new(&args[1]))?;
-    println!("{:?}", result);
+    //println!("country_file;total_population;weighted_lat;weighted_lon");
+    if result.total_population > 0.0 {
+        println!("{};{:.15};{:.15};{:.15}", &args[1], result.total_population, result.weighted_lat, result.weighted_lon);
+    }
     Ok(())
 }
