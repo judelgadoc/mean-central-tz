6a4dd8bc6a918e8a7394698077b171ba3a5cac27
Juan Pablo Delgado <judelgadoc@unal.edu.co>
2025-08-14 11:35:20 +0800

    Refactoring
    Also imporved fallback downloader

--- data/fallback_downloader.py
+++ data/fallback_downloader.py
@@ -23,31 +23,12 @@ countries = ['ABW', 'AFG', 'AGO', 'AIA', 'ALA', 'ALB', 'AND', 'ARE', 'ARG', 'ARM
              'VIR', 'VNM', 'VUT', 'WLF', 'WSM', 'XDI', 'XIB', 'XIK', 'XKX', 'XMA', 'XSI', 'YEM', 
              'ZAF', 'ZMB', 'ZWE']
 
-countries = ['CUB', 'CUW', 'CXR', 'CYM', 'CYP', 'CZE', 'DEU', 
-             'DJI', 'DMA', 'DNK', 'DOM', 'DZA', 'ECU', 'EGY', 'ERI', 'ESH', 'ESP', 'EST', 'ETH', 
-             'FIN', 'FJI', 'FLK', 'FRA', 'FRO', 'FSM', 'GAB', 'GBR', 'GEO', 'GGY', 'GHA', 'GIB', 
-             'GIN', 'GLP', 'GMB', 'GNB', 'GNQ', 'GRC', 'GRD', 'GRL', 'GTM', 'GUF', 'GUM', 'GUY', 
-             'HKG', 'HMD', 'HND', 'HRV', 'HTI', 'HUN', 'IDN', 'IMN', 'IND', 'IOT', 'IRL', 'IRN', 
-             'IRQ', 'ISL', 'ISR', 'ITA', 'JAM', 'JEY', 'JOR', 'JPN', 'KAZ', 'KEN', 'KGZ', 'KHM', 
-             'KIR', 'KNA', 'KOR', 'KWT', 'LAO', 'LBN', 'LBR', 'LBY', 'LCA', 'LIE', 'LKA', 'LSO', 
-             'LTU', 'LUX', 'LVA', 'MAC', 'MAF', 'MAR', 'MCO', 'MDA', 'MDG', 'MDV', 'MEX', 'MHL', 
-             'MKD', 'MLI', 'MLT', 'MMR', 'MNE', 'MNG', 'MNP', 'MOZ', 'MRT', 'MSR', 'MTQ', 'MUS', 
-             'MWI', 'MYS', 'MYT', 'NAM', 'NCL', 'NER', 'NFK', 'NGA', 'NIC', 'NIU', 'NLD', 'NOR', 
-             'NPL', 'NRU', 'NZL', 'OMN', 'PAK', 'PAN', 'PCN', 'PER', 'PHL', 'PLW', 'PNG', 'POL', 
-             'PRI', 'PRK', 'PRT', 'PRY', 'PSE', 'PYF', 'QAT', 'REU', 'ROU', 'RUS', 'RWA', 'SAU', 
-             'SDN', 'SEN', 'SGP', 'SGS', 'SHN', 'SJM', 'SLB', 'SLE', 'SLV', 'SMR', 'SOM', 'SPM', 
-             'SRB', 'SSD', 'STP', 'SUR', 'SVK', 'SVN', 'SWE', 'SWZ', 'SXM', 'SYC', 'SYR', 'TCA', 
-             'TCD', 'TGO', 'THA', 'TJK', 'TKL', 'TKM', 'TLS', 'TON', 'TTO', 'TUN', 'TUR', 'TUV', 
-             'TWN', 'TZA', 'UGA', 'UKR', 'UMI', 'URY', 'USA', 'UZB', 'VAT', 'VCT', 'VEN', 'VGB', 
-             'VIR', 'VNM', 'VUT', 'WLF', 'WSM', 'XDI', 'XIB', 'XIK', 'XKX', 'XMA', 'XSI', 'YEM', 
-             'ZAF', 'ZMB', 'ZWE']
-
-
-url = lambda c: "https://data.worldpop.org/GIS/Population/Global_2015_2030/R2024B/2016/{0}/v1/1km_ua/constrained/{1}_pop_2016_CN_1km_R2024B_UA_v1.tif".format(c, c.lower())
+year = 2016
+url = lambda c: "https://data.worldpop.org/GIS/Population/Global_2015_2030/R2024B/{2}/{0}/v1/1km_ua/constrained/{1}_pop_{2}_CN_1km_R2024B_UA_v1.tif".format(c, c.lower(), year)
 
 for i in countries:
     r = requests.get(url(i), allow_redirects=True)
     name = url(i).split("/")[-1]
     print(name)
-    open(f"1km/2016/{name}", 'wb').write(r.content)
+    open(f"1km/{year}/{name}", 'wb').write(r.content)
 
